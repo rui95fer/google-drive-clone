b420082aff3b5ce5966f6eead30d9bf21b150dce
Rui <rui95fer@gmail.com>
2024-11-25 15:43:26 +0000

    feat(routes): define main application routes

--- resources/js/Components/app/Navigation.vue
+++ resources/js/Components/app/Navigation.vue
@@ -7,19 +7,25 @@ import NavLink from '@/Components/app/NavLink.vue';
 
 <template>
     <nav class="min-w-52">
-        <div class="flex h-20 items-center gap-3 px-3">
-            <Link href="/">
-                <ApplicationLogo
-                    class="block h-9 w-auto fill-current text-gray-800"
-                />
-                LaraStorage
-            </Link>
-        </div>
+        <Link
+            class="flex h-20 items-center gap-3 px-3"
+            :href="route('myFiles')"
+        >
+            <ApplicationLogo
+                class="block h-9 w-auto fill-current text-gray-800"
+            />
+            LaraStorage
+        </Link>
+
         <div class="px-3">
             <CreateNewDropdown />
 
             <div class="py-3">
-                <NavLink href="/">My Files</NavLink>
+                <NavLink
+                    :href="route('myFiles')"
+                    :active="route().current('myFiles')"
+                    >My Files
+                </NavLink>
                 <NavLink href="/">Shared With Me</NavLink>
                 <NavLink href="/">Shared By Me</NavLink>
                 <NavLink href="/">Trash</NavLink>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FileController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -14,6 +15,12 @@ Route::get('/', function () {
     ]);
 });
 
+Route::controller(FileController::class)
+    ->middleware(['auth', 'verified'])
+    ->group(function () {
+        Route::get('/my-files', 'myFiles')->name('myFiles');
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -24,4 +31,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
